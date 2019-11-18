c080dd7862161f0aef9d91871f993837e9e8e970
Kevin <kevin.ceriati@le-campus-numerique.fr>
2019-11-18 11:23:05 +0100

    first commit

---
+++ src/test/java/com/rottencar/restservice/RestApp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rottencar.restservice.RestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
