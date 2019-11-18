e20c72ca9a8f04400b256d8f94d76600353b04be
Kevin <kevin.ceriati@le-campus-numerique.fr>
2019-11-18 15:41:15 +0100

    fin itération 2

---
+++ src/main/java/com/rottencar/restservice/RestApp/controller/MainController.java
@@ -0,0 +1,91 @@
+package com.rottencar.restservice.RestApp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.rottencar.restservice.RestApp.form.CarForm;
+import com.rottencar.restservice.RestApp.model.Car;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+
+    private static List<Car> cars = new ArrayList<>();
+
+    static {
+        cars.add(new Car("Multipla", "Fiat"));
+        cars.add(new Car("Robin", "Reliant"));
+    }
+
+    // Injectez (inject) via application.properties.
+    @Value("${welcome.message}")
+    private String message;
+
+    @Value("${error.message}")
+    private String errorMessage;
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("message", message);
+
+        return "index";
+    }
+
+    /**
+     *
+     * @param model
+     * @return carList
+     */
+    @RequestMapping(value = { "/carList" }, method = RequestMethod.GET)
+    public String carList(Model model) {
+
+        model.addAttribute("cars", cars);
+
+        return "carList";
+    }
+
+    /**
+     *
+     * @param model
+     * @return addCar
+     */
+    @RequestMapping(value = { "/addCar" }, method = RequestMethod.GET)
+    public String showAddCarPage(Model model) {
+
+        CarForm carForm = new CarForm();
+        model.addAttribute("carForm", carForm);
+
+        return "addCar";
+    }
+
+    /**
+     * @param model
+     * @param carForm
+     * @return carList
+     */
+    @RequestMapping(value = { "/addCar" }, method = RequestMethod.POST)
+    public String saveCar(Model model, //
+                             @ModelAttribute("carForm") CarForm carForm) {
+
+        String name = carForm.getName();
+        String brand = carForm.getBrand();
+
+        if (name != null && name.length() > 0 //
+                && brand != null && brand.length() > 0) {
+            Car newCar = new Car(name, brand);
+            cars.add(newCar);
+
+            return "redirect:/carList";
+        }
+
+        model.addAttribute("errorMessage", errorMessage);
+        return "addCar";
+    }
+
+}
---
+++ src/main/java/com/rottencar/restservice/RestApp/form/CarForm.java
@@ -0,0 +1,23 @@
+package com.rottencar.restservice.RestApp.form;
+
+public class CarForm {
+
+    private String name;
+    private String brand;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
